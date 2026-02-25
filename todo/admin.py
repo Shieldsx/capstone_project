ee17b8760a8b65e02e8c9919f8f151fe79c1f9f0
Frank <fredhltomlinson@gmail.com>
2026-02-25 09:35:40 +0000

    Registered TodoList and Task models in Django admin

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import TodoList, Task
 
-# Register your models here.
+@admin.register(TodoList)
+class TodoListAdmin(admin.ModelAdmin):
+    list_display = ("name", "owner", "updated_on", "created_on")
+    search_fields = ("name", "owner__username", "owner__email")
+    list_filter = ("created_on", "updated_on")
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("title", "todo_list", "completed", "updated_on", "created_on")
+    search_fields = ("title", "todo_list__name", "todo_list__owner__username")
+    list_filter = ("completed", "created_on", "updated_on")
